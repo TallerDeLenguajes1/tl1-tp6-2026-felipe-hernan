e2336503079d8556084cbe81080fbef4ecc3dc9e
felipeh <hhh.hb38@gmail.com>
2026-05-23 13:43:26 -0300

    fin ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Hello, World!");
+int a;
+int b;
+a = 10;
+b = a;
+Console.WriteLine("Valor de a: "+a);
+Console.WriteLine("Valor de B: "+b);
+int i = 0;
+int inver = 0;
+string entrada = Console.ReadLine();
+if (int.TryParse(entrada,out i))
+{
+    while (i>0) //invierte el numero digito a digito
+    {
+        inver =inver*10 +i%10; 
+        i/=10;
+    }
+}
+if (inver>0)
+{
+    Console.WriteLine("Numero invertido "+inver);
+}
+else
+{
+    Console.WriteLine("No es posible invertir");
+} 
