d90984238ecfee1a65a80246a974bba81e527e32
felipeh <hhh.hb38@gmail.com>
2026-05-23 20:49:06 -0300

    Calculadora V1

--- Program.cs
+++ Program.cs
@@ -1,26 +1,93 @@
-﻿Console.WriteLine("Hello, World!");
-int a;
-int b;
-a = 10;
-b = a;
-Console.WriteLine("Valor de a: "+a);
-Console.WriteLine("Valor de B: "+b);
-int i = 0;
-int inver = 0;
-string entrada = Console.ReadLine();
-if (int.TryParse(entrada,out i))
+﻿//calculadora version 1
+int opcion1 = 0;
+int opcion2 = 0;
+decimal numeroA = 1;
+decimal numeroB = 1;
+string Num1 = "";
+bool esUnNumeroValido = false;
+
+do
 {
-    while (i>0) //invierte el numero digito a digito
+    Console.WriteLine("Ingrese una opcion:");
+    Console.WriteLine("1 - Sumar");
+    Console.WriteLine("2 - Restar");
+    Console.WriteLine("3 - Multiplicar");
+    Console.WriteLine("4 - Dividir");
+    
+    while(!esUnNumeroValido)
     {
-        inver =inver*10 +i%10; 
-        i/=10;
+        Num1 = Console.ReadLine();
+        esUnNumeroValido = int.TryParse(Num1,out opcion1);
+        if (!esUnNumeroValido)
+        {
+            Console.WriteLine(Num1 +" no es un numero valido");
+            Console.WriteLine("Ingrese una opcion:");
+        }
     }
-}
-if (inver>0)
+    if (opcion1<1 || opcion1>4) //comprueba que es un un numero y que sea una opcion valida
+    {
+        Console.WriteLine("Opcion no valida ");
+        esUnNumeroValido = false;
+    }
+
+} while (opcion1 == 0 || opcion1<1 || opcion1>4);
+
+do
 {
-    Console.WriteLine("Numero invertido "+inver);
-}
-else
-{
-    Console.WriteLine("No es posible invertir");
-} 
+    esUnNumeroValido = false;
+    while(!esUnNumeroValido)
+    {
+        Console.WriteLine("Ingrese el primer numero: ");
+        Num1 = Console.ReadLine();
+        esUnNumeroValido = decimal.TryParse(Num1,out numeroA);
+        if (!esUnNumeroValido)
+        {
+            Console.WriteLine(Num1 +" no es un numero valido");
+        }
+    }
+    esUnNumeroValido = false;
+    while(!esUnNumeroValido || numeroB == 0)
+    {
+        Console.WriteLine("Ingrese el segundo numero: ");
+        Num1 = Console.ReadLine();
+        esUnNumeroValido = decimal.TryParse(Num1,out numeroB);
+        if (!esUnNumeroValido)
+        {
+            Console.WriteLine(Num1 +" no es un numero valido");
+            
+        }
+        if (numeroB == 0)
+        {
+            Console.WriteLine("No puede ser 0");
+        }
+    }
+
+    switch (opcion1)
+    {
+        case 1:
+
+            Console.WriteLine("La suma es : "+(numeroA+numeroB));
+            break;
+        case 2:
+            Console.WriteLine("La resta es : "+(numeroA-numeroB));
+            break;
+        case 3:
+            Console.WriteLine("La multiplicacion es : "+(numeroA*numeroB));
+            break;
+        case 4:
+            Console.WriteLine("La divicion es : "+(numeroA/numeroB));
+            break;
+    }
+    esUnNumeroValido = false;
+    while(!esUnNumeroValido)
+    {
+        Console.WriteLine("Desea realizar otro calculo? si = 1, no = 0");
+        Num1 = Console.ReadLine();
+        esUnNumeroValido = int.TryParse(Num1,out opcion2);
+        if (!esUnNumeroValido)
+        {
+            Console.WriteLine(Num1 +" no es un numero valido");
+        }
+    }
+
+} while (opcion2 == 1);
