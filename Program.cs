558607ce79ef68659bb08ef5c34c24c1cd4a46b2
felipeh <hhh.hb38@gmail.com>
2026-05-24 14:04:29 -0300

    Ejercicio 4 terminado

--- Program.cs
+++ Program.cs
@@ -33,7 +33,60 @@ Console.WriteLine($@"La longitud de la cadena de texto es {cadena1.Length}");
 Console.WriteLine("ingrese otro texto");
 string cadena2 = Console.ReadLine();
 Console.WriteLine($@"Dos cadena {cadena1} {cadena2}");
-Console.WriteLine($@"parte de una cadena {cadena1.Substring(3,4)}");
+Console.WriteLine($@"parte de una cadena {cadena1.Substring(2,2)}");
+
+foreach (char item in cadena1)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("Cadena en minuscula "+cadena1.ToLower());
+Console.WriteLine("Cadena en mayuscula "+cadena1.ToUpper());
+Console.WriteLine("ingrese un texto separado por -");
+cadena1 = Console.ReadLine();
+string[] nuevaCadena = cadena1.Split('-');
+foreach (var item in nuevaCadena) //escribe las subcadenas
+{
+    Console.WriteLine(item);
+}
+decimal numeroA = 0;
+decimal numeroB = 0;
+bool esUnNumeroValido;
+char operacion =' ';
+string aux = " ";
+
+Console.WriteLine("ingrese una operacion a realizar");
+cadena1 = Console.ReadLine();
+nuevaCadena = cadena1.Split('-','*','/','+');//separa las cadenas por algun operador
+
+foreach (char operador in cadena1) //busca la operacion a realizar
+{
+    if (operador == '-' || operador == '*' ||operador == '/' ||operador == '+' )
+    {
+        operacion = operador;
+        break;
+    }
+}
+
+aux =  nuevaCadena[0];
+esUnNumeroValido = decimal.TryParse(aux,out numeroA);
+aux =  nuevaCadena[1];
+esUnNumeroValido = decimal.TryParse(aux,out numeroB);
+
+switch (operacion) //realiza las operaciones
+{
+    case '+':
+        Console.WriteLine("La suma es : "+(numeroA+numeroB));
+        break;
+    case '-':
+        Console.WriteLine("La resta es : "+(numeroA-numeroB));
+        break;
+    case '*':
+        Console.WriteLine("La multiplicacion es : "+(numeroA*numeroB));
+        break;
+    case '/':
+        Console.WriteLine("La divicion es : "+(numeroA/numeroB));
+        break;
+}
 
 
 
