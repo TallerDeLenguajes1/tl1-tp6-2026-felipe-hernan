8b8536bfe2dc0c67f250ff3de22782647cf5a0f1
felipeh <hhh.hb38@gmail.com>
2026-05-24 11:38:33 -0300

    parte del ejercicio 4

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ Console.WriteLine("Valor de a: "+a);
 Console.WriteLine("Valor de B: "+b);
 int i = 0;
 int inver = 0;
+Console.WriteLine("Ingrese un numero: ");
 string entrada = Console.ReadLine();
 if (int.TryParse(entrada,out i))
 {
@@ -24,3 +25,15 @@ else
 {
     Console.WriteLine("No es posible invertir");
 } 
+
+//ejercicio 4
+Console.WriteLine("ingrese un texto");
+string cadena1 = Console.ReadLine();
+Console.WriteLine($@"La longitud de la cadena de texto es {cadena1.Length}");
+Console.WriteLine("ingrese otro texto");
+string cadena2 = Console.ReadLine();
+Console.WriteLine($@"Dos cadena {cadena1} {cadena2}");
+Console.WriteLine($@"parte de una cadena {cadena1.Substring(3,4)}");
+
+
+
